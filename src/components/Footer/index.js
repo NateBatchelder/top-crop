74c2a8539acd3cb78381db086c7eb2c4f851e9ec
Nathan Batchelder <natebatchelder@gmail.com>
2022-03-12 14:04:15 -0800

    removed boilerplate css/ added bootstrap elements to nav and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,42 +4,18 @@ import {
   MDBCardBody,
   MDBCardTitle,
   MDBCardText,
-  MDBCardHeader,
   MDBBtn,
 } from "mdb-react-ui-kit";
 
 function Footer() {
   return (
-    <footer>
-      
-          <MDBCard
-            alignment="center"
-            style={{ width: "100%", margin: "0 auto" }}
-          >
-            <MDBCardHeader>
-              <MDBCardTitle>
-                <h1>Contact</h1>
-              </MDBCardTitle>
-            </MDBCardHeader>
-            <MDBCardBody>
-              <MDBCardText>
-                <p>some text</p>
-              </MDBCardText>
-            </MDBCardBody>
-            
-              <MDBBtn color="primary">
-                <a
-                  href="linkedin.com/in/nathanbatchelder"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </MDBBtn>
-           
-          </MDBCard>
-     
-    </footer>
+    <MDBCard alignment="center">
+      <MDBCardBody>
+        <MDBCardTitle>&copy; Forked Lightning 2022</MDBCardTitle>
+        <MDBCardText>Nate Batchelder</MDBCardText>
+        <MDBBtn href="#">LinkedIn</MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
   );
 }
 
--- src/components/TopCrop/index.js
+++ src/components/TopCrop/index.js
@@ -1,27 +1,62 @@
-import React from 'react';
+import React from "react";
+import { MDBProgress, MDBProgressBar } from "mdb-react-ui-kit";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBRow,
+  MDBCol,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
-function Body() {
+function Main() {
   return (
-    // row
-    <div>
-      {/* column */}
-      <div className="col-1-of-2">
-        <h1>User Input</h1>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" className="form-control" />
-        </div>
-      </div>
-      {/* column */}
-      <div className="col-2-of-2">
-        <h1>Application Output</h1>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" className="form-control" />
-        </div>
-      </div>
-    </div>
+    <main>
+      <section>
+        <h1>Get Generating</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </section>
+      <section>
+        <MDBRow>
+          <MDBCol sm="6">
+            <MDBCard>
+              <MDBCardBody>
+                <MDBCardTitle>Special title treatment</MDBCardTitle>
+                <MDBCardText>
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </MDBCardText>
+                <MDBBtn href="#">Go somewhere</MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol sm="6">
+            <MDBCard>
+              <MDBCardBody>
+                <MDBCardTitle>Special title treatment</MDBCardTitle>
+                <MDBCardText>
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </MDBCardText>
+                <MDBBtn href="#">Go somewhere</MDBBtn>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </section>
+      <article>
+        <MDBProgress>
+          <MDBProgressBar
+            className="w-75"
+            valuenow={75}
+            valuemin={0}
+            valuemax={100}
+          />
+        </MDBProgress>
+      </article>
+    </main>
   );
 }
 
-export default Body;
+export default Main;
